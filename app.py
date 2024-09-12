14df86555f06d0badd28c54c39346e9243b718c9
pericles.savio <pericles.savio@redecredauto.com.br>
2024-09-12 09:30:02 -0300

    inicio

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+pegadinhas = {
+    'pegadinha1': False,
+    'pegadinha2': False,
+    'pegadinha3': False,
+    'pegadinha4': False
+}
+
+@app.route('/pegadinha', methods=['GET'])
+def get_pegadinhas():
+    return jsonify(pegadinhas)
+
+@app.route('/pegadinha', methods=['POST'])
+def set_pegadinha():
+    global pegadinhas
+    data = request.get_json()
+    if all(key in data and isinstance(data[key], bool) for key in pegadinhas):
+        pegadinhas.update(data)
+        return jsonify(pegadinhas), 200
+    else:
+        return jsonify({'error': 'Todos os campos devem ser true ou false'}), 400
+
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
